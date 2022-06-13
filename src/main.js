a3b6a6171a8a9cf91e00ff6ec72b57dd2da15b79
Boris <boris260692@gmail.com>
2022-06-13 19:26:16 +0300

    Added block rest/spread

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './styles/main.scss';
 import './bootstrap.js';
+import './user.js'
 import $ from "jquery"; // бібліотека jquery
---
+++ src/user.js
@@ -0,0 +1,16 @@
+// Появлення блоків
+let block1 = document.querySelector('.first')
+let block2 = document.querySelector('.two')
+let block3 = document.querySelector('.three')
+let block4 = document.querySelector('.four')
+let block5 = document.querySelector('.five')
+let block6 = document.querySelector('.six')
+
+setTimeout(() => block1.classList.add('fadeIn'),500)
+setTimeout(() => block2.classList.add('fadeIn'),500)
+setTimeout(() => block3.classList.add('fadeIn'),500)
+setTimeout(() => block4.classList.add('fadeIn'),500)
+setTimeout(() => block5.classList.add('fadeIn'),500)
+setTimeout(() => block6.classList.add('fadeIn'),500)
+
+
